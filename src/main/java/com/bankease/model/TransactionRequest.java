6356ac01f5da494f8c855e05497536d44395cfa9
Basva Thriveni <thrivenibasva@gmail.com>
2026-09-11 15:38:44 +0530

    Update TransactionRequest.java

--- src/main/java/com/bankease/model/TransactionRequest.java
+++ src/main/java/com/bankease/model/TransactionRequest.java
@@ -1,1 +1,19 @@
+package com.bankease.model;
 
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Positive;
+
+import java.math.BigDecimal;
+
+public class TransactionRequest {
+    @NotNull private Long fromAccount;
+    @NotNull private Long toAccount;
+    @NotNull @Positive private BigDecimal amount;
+
+    public Long getFromAccount() { return fromAccount; }
+    public void setFromAccount(Long fromAccount) { this.fromAccount = fromAccount; }
+    public Long getToAccount() { return toAccount; }
+    public void setToAccount(Long toAccount) { this.toAccount = toAccount; }
+    public BigDecimal getAmount() { return amount; }
+    public void setAmount(BigDecimal amount) { this.amount = amount; }
+}
